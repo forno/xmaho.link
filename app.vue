141620c5e503e6be31f4992bac9d8d578cc498f6
FORNO <forno@xmaho.link>
2022-08-25 18:14:04 +0900

    Update one
    Signed-off-by: FORNO <forno@xmaho.link>

--- app.vue
+++ app.vue
@@ -35,22 +35,24 @@ uniform float iTime; // shader playback time (in seconds)
 varying vec2 vUv;
 
 void main () {
-    gl_FragColor = vec4(0., 0., 0., 1.);
+  gl_FragColor = vec4(0., 0., 0., 1.);
 
-    //Resolution for scaling
-    vec2 R = iResolution.xy;
-    //Loop through 30 orbs
-    for(float i=0.; i<30.; ++i)
-        //Color (simple sine palette)
-        gl_FragColor += (sin(i+vec4(0,1,2,0)) + 1.)/.3
-        //Brightness attenuation
-            / length(R*(2.*vUv-1.+cos(i*R+iTime)));
+  //Resolution for scaling
+  vec2 R = iResolution.xy;
+  //Loop through 30 orbs
+  for(float i=0.; i<30.; ++i) {
+    //Color (simple sine palette)
+    gl_FragColor += (sin(i+vec4(0,1,2,0)) + 1.)/.3
+      / length(R*(2.*vUv-1.+cos(i*R+iTime))); //Brightness attenuation
+  }
 }
 `,
     uniforms: {
       ...ShaderLib.background.uniforms,
       iTime: { value: 0 },
-      iResolution: new Uniform(new Vector2()),
+      iResolution: new Uniform(
+        new Vector2(globalThis.innerWidth, globalThis.innerHeight)
+      ),
     },
   });
   const bgMesh = new Mesh(new PlaneGeometry(2, 2), bgMaterial);
@@ -61,10 +63,7 @@ void main () {
     globalThis.requestAnimationFrame(animate);
 
     bgMaterial.uniforms.iTime.value = clock.getElapsedTime();
-    bgMaterial.uniforms.iResolution.value = new Vector2(
-      globalThis.innerWidth,
-      globalThis.innerHeight
-    );
+
     renderer.render(scene, camera);
   };
 
