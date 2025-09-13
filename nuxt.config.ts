eeb57055eea5de5d06d906d3ff843432d27ecc90
FORNO <forno@xmaho.link>
2025-09-14 01:02:28 +0900

    feat: amplify.ymlのビルドコマンドとアーティファクトの出力先を変更
    ビルドコマンドを`npm run generate`から`npm run build`に変更し、
    アーティファクトの出力先を`.output/public`から`.amplify-hosting`に修正しました。
    これにより、ビルドプロセスが正しく機能し、アーティファクトが期待通りの場所に出力されるようになります。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@unocss/nuxt'],
-  nitro: {
-    preset: 'static',
-  },
   compatibilityDate: '2025-09-14',
 })
